28293aa31492e5da5bb54b1f5d0f9649030d9790
d22hanol <d22hanol@student.his.se>
2023-09-19 18:10:09 +0200

    kod klar men funkar ej

--- SQL-database.sql
+++ SQL-database.sql
@@ -1,3 +1,4 @@
+drop database d22hanol;
 create database d22hanol;
 use d22hanol;
 
@@ -7,27 +8,33 @@ create table Incident(
     plats varchar(16),
     primary key (namn,nr)
 )engine=innodb;
+/* -----Coding____*/
+
 
 create table PåbörjadOperation(
   kodnamnTyp varchar(32),
+  kodnamnsKod int unique auto_increment,
   startdatum date,
   incidentNamn varchar(16),
   incidentNr char(4),
   foreign key (incidentNamn) references Incident(namn),
-  foreign key (incidentNr) references Incident(Nr),
+  foreign key (incidentNr) references Incident(nr),
   primary key (kodnamnTyp,startdatum,incidentNr,incidentNamn)
 )engine =innodb;
 
+
 create table SlutfördOperation(
   kodnamnTyp varchar(32),
+  kodnamnskod int unique auto_increment ,
   startdatum date,
   slutdatum date,
-  successRate char(1),
+  successRate bit ,
   incidentNamn varchar(16),
   incidentNr char(4),
   foreign key (incidentNamn) references Incident(namn),
   foreign key (incidentNr) references Incident(Nr),
-  primary key (kodnamnTyp,startdatum,incidentNr,incidentNamn)
+  primary key (kodnamnTyp,startdatum,incidentNr,incidentNamn),
+  check ( successRate = 1 or 2 or 3 or 4)
 )engine =innodb;
 
 create table Observation(
@@ -42,23 +49,38 @@ create table Observation(
     foreign key (incidentNr) references Incident(Nr)
 )engine= innodb;
 
-create index ObservationsID on Observation(ID asc) using btree;
+/* ----Merge ___*/ 
 
-create table alien(
+create table alienrymdskepp(
+    alien boolean,
+    rymdskepp boolean,
     Hudfärg char(8),
     kläder varchar(25),
     typ varchar(25),
     storlek char (3),
-    observation char(8),
-    foreign key (observation) references Observation(ID),
-    primary key (observation)
-)engine=innodb;
-
-create table rymdskepp(
     form char(8),
     lampor char(12),
     färg char(8),
     rörelse varchar(25),
+    observation char(8),
+    foreign key (observation) references Observation(ID),
+    primary key (observation),
+    check ( if (alien = 1,
+        rymdskepp = 0,
+        form = NULL,
+        lampor = NULL,
+        färg = NULL,
+        rörelse = NULL)),
+    check ( if ( rymdskepp = 1,
+        alien = 0,
+        Hudfärg = NULL,
+        kläder = NULL,
+        typ = NULL,
+        storlek = NULL))
+)engine=innodb;
+
+create table rymdskepp(
+
     observation char(8),
     foreign key (observation) references Observation(id),
     primary key (observation)
@@ -71,6 +93,16 @@ create table Media(
     foreign key (observation) references Observation(ID),
     primary key (namn,observation)
 )engine= innodb;
+
+create table MediaLogg(
+    ID smallint unsigned auto_increment,
+    operation varchar(10),
+    användarnamn varchar(32),
+    nyaNamn varchar(10),
+    gammalNamn varchar(10),
+    optime datetime,
+    primary key (ID)
+)engine=innodb;
 
 create table MediaKommentar(
     kommentar varchar(128),
@@ -95,5 +127,55 @@ create table PersonPåObservation(
     foreign key (observation) references Observation(ID)
 )engine=innodb;
 
+/* Index */
+create index Observationsdatum on Observation(datum asc) using btree;
 
+create index PåbörjadOperationsdatum on PåbörjadOperation(startdatum asc) using btree;
+/* View */
+Create VIEW BådaOperationer as
+select kodnamnTyp,kodnamnsKod, startdatum, Null as "slutdatum", Null as "successRate", incidentNamn, incidentNr
+from PåbörjadOperation
+union
+select kodnamnTyp,kodnamnskod, startdatum, slutdatum, successRate, incidentNamn, incidentNr
+from SlutfördOperation;
+
+create VIEW FullständigMedia as
+select namn, kvalite, observation, Null as "kommentar"
+from Media
+union
+select namn, Null as "kvalite", observation, kommentar
+from MediaKommentar;
+
+delimiter //
+ /* procedure */
+create procedure HämtaAllaPersoner()
+begin
+    select * from Person;
+end //
+
+
+create procedure HämtaOperationsnamn(in prod smallint)
+begin
+    select kodnamnTyp
+    from BådaOperationer
+        where kodnamnsKod=prod;
+end //
+ /* Trigger */
+create trigger MediaTriggerInsert after insert on Media
+    for each row begin
+    insert into MediaLogg(operation, användarnamn, nyaNamn,gammalNamn, optime)
+        values ('Insert',user(),new.namn,'-',now());
+end //
+
+create trigger MediaTriggerDelete after update on Media
+    for each row begin
+    insert into MediaLogg(operation, användarnamn, nyaNamn,gammalNamn, optime)
+        values ('Update',user(),new.namn,old.namn,now());
+end //
+
+delimiter ;
+ /* Rättigheter */
+create user Gruppledare identified by 'Lösenord';
+grant update on d22hanol.PåbörjadOperation to Gruppledare;
+grant update on d22hanol.SlutfördOperation to Gruppledare;
 
